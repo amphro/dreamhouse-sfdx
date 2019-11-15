4af61250385d64e4bf32abb8d15ab2f07ba502d9
Thomas <t.dvornik@salesforce.com>
2019-11-14 16:41:54 -0800

    move stuff around

--- scripts/extract-issue-number.js
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env node
-
-const issueUrl = process.argv[2];
-process.stdout.write(issueUrl.substring(issueUrl.lastIndexOf('/') + 1, issueUrl.length));
--- scripts/orgInit.sh
+++ scripts/orgInit.sh
@@ -3,11 +3,18 @@
 DURATION=7
 
 if [ "$#" -eq 1 ]; then
-  DURATION=$1
+  sfdx force:org:create -a dreamhouse -s -f config/project-scratch-def.json -d $DURATION username=$1
+else
+  sfdx force:org:create -a dreamhouse -s -f config/project-scratch-def.json -d $DURATION
 fi
 
-sfdx force:org:create -a dreamhouse -s -f config/project-scratch-def.json -d $DURATION
 sfdx force:source:push
 sfdx force:user:permset:assign -n dreamhouse
-sfdx force:org:open -p /lightning/page/home
+sfdx force:data:tree:import -p data/sample-data-plan.json
+
+# Only open when a user is running the script, not in CI
+if [ "$GITHUB_WORKFLOW" == "" ]; then
+  sfdx force:org:open -p /lightning/page/home
+fi
+
 echo "Org is set up"
