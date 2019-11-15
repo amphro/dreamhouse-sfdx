62c478f3f3ad9b70955d66446618f490c15eb3db
Thomas <t.dvornik@salesforce.com>
2019-11-15 14:34:16 -0800

    update script

--- scripts/sendEmail.sh
+++ scripts/sendEmail.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-DISPLAY_USER_JSON=$(sfdx force:user:display --json)
+export DISPLAY_USER_JSON=$(sfdx force:user:display --json)
 echo $DISPLAY_USER_JSON
-DISPLAY_URL_JSON=$(sfdx force:org:open --urlonly --json)
+export DISPLAY_URL_JSON=$(sfdx force:org:open --urlonly --json)
 echo $DISPLAY_URL_JSON
+
+scripts/generateSendEmailScript.js
 
 sfdx force:apex:execute -f scripts/sendEmail.apex
